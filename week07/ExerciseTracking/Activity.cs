dd5643dee6b2e7d17c0f0c99ddeafcdbb09466b5
SilviaCastagnino <sylvia19976@gmail.com>
2025-06-17 10:59:13 +0200

    New files

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,30 @@
+public abstract class Activity
+{
+    private DateTime _date;
+    protected int _minutes;
+
+
+    public Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToString("dd MMM yyyy")} {GetType().Name} ({_minutes} min) - " + $"Distance: {GetDistance():F1} miles, Speed: {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2025, 06, 17), 30, 3.0),
+            new Cycling(new DateTime(2025, 06, 17), 30, 9.7),
+            new Swimming(new DateTime(2025, 06, 17), 30, 20)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
